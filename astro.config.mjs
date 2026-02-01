a9821eb713918246bae97e5e45d00093e130d89c
ayushdeo <mailbox4ayushxxx@gmail.com>
2026-01-31 23:35:29 -0800

    Configure GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ayushdeo.github.io',
+  base: '/Persona_Portfolio',
   integrations: [tailwind(), react()]
 });
